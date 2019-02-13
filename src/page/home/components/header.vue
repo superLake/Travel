66a794e6c8110082732297ab8f57cc9244065da9
superLake <404982545@qq.com>
2019-02-13 18:08:45 +0800

    headerv1.0

---
+++ src/page/home/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+
+</script>
+<style lang="stylus" scoped>
+  .header
+    display:flex
+    line-height: .86rem
+    background: #00bcd4
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+      height:.64rem
+      line-height:.64rem
+      margin:.12rem .2rem 0 0
+      background:#fff
+      border-radius:.1rem
+      color:#ccc
+    .header-right
+      width: 1.24rem
+      float: right
+</style>
