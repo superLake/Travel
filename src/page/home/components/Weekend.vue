676b7ec103bd207b31d9b4aa67c7360428356de8
superLake <404982545@qq.com>
2019-02-18 11:09:56 +0800

    'recommend'

---
+++ src/page/home/components/Weekend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="recommend">
+    <div>
+      <div class="title">周末去哪儿</div>
+      <ul>
+        <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl" alt=""/>
+          </div>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default{
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: 1,
+        title: '清远周边逛',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        desc: '人人都有一双发现美的眼睛'
+      }, {
+        id: 2,
+        title: '清远周边逛',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        desc: '人人都有一双发现美的眼睛'
+      }, {
+        id: 3,
+        title: '清远周边逛',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        desc: '人人都有一双发现美的眼睛'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~style/mixins.styl'
+  .title
+    background:#eee
+    margin-top:.2rem
+    line-height :.8rem
+    text-indent :.2rem
+  .item-img-wrapper
+    overflow:hidden
+    height :0
+    padding-bottom: 33.7%
+    .item-img
+      width :100%
+      padding :.1rem
+  .item-info
+    padding:.1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipse()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipse()
+</style>
--- src/page/home/components/recommend.vue
+++ src/page/home/components/recommend.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="recommend">
     <div>
-      <div class="title"></div>
+      <div class="title">热门推荐</div>
       <ul>
-        <li class="item">
-          <img class="item-img" src="" alt=""/>
-          <p class="item-title"></p>
-          <p class="item-desc"></p>
-          <buttom class="item-buttom"></buttom>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+          <img class="item-img" :src="item.imgUrl" alt=""/>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
         </li>
       </ul>
     </div>
@@ -15,9 +17,61 @@
 </template>
 <script>
 export default{
-
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: 1,
+        title: '深圳野生动物园',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/2e/d9363ce50a88c8c5ff5a138c67687cd4.water.jpg_200x200_1c15c8f5.jpg',
+        desc: '去旅游，就来深圳野生动物园'
+      }, {
+        id: 2,
+        title: '深圳野生动物园',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/2e/d9363ce50a88c8c5ff5a138c67687cd4.water.jpg_200x200_1c15c8f5.jpg',
+        desc: '去旅游，就来深圳野生动物园'
+      }, {
+        id: 3,
+        title: '深圳野生动物园',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/2e/d9363ce50a88c8c5ff5a138c67687cd4.water.jpg_200x200_1c15c8f5.jpg',
+        desc: '去旅游，就来深圳野生动物园'
+      }]
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
-
+@import '~style/mixins.styl'
+.title
+  background:#eee
+  margin-top:.2rem
+  line-height :.8rem
+  text-indent :.2rem
+.item
+  display: flex
+  height: 1.9rem
+  overflow: hidden
+  .item-img
+    width :1.7rem
+    height :1.7rem
+    padding :.1rem
+  .item-info
+    min-width:0
+    flex:1
+    padding:.1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipse()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipse()
+    .item-button
+      line-height :.44rem
+      margin-top: .16rem
+      background: #ff9300
+      padding: 0 .2rem
+      border-radius :.06rem
+      color: #fff
 </style>
