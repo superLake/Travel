de0964fe191969b8225def0d96127a9b58fad211
superLake <404982545@qq.com>
2019-02-27 22:40:40 +0800

    city finish

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <!-- 显示的是当前路由地址所对应的的内容 -->
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -15,6 +15,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/recommend'
 import HomeWeekend from './components/Weekend'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -29,12 +30,16 @@ export default {
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      lastCity: ''
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo () {
-      axios.get('./api/index.json')
+      axios.get('./api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -48,7 +53,14 @@ export default {
       }
     }
   },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+  },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
   }
 }
